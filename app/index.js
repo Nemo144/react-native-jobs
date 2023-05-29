cd84229549fb460bb75ff7af0eded556b75ceffa
Nnamdi Okeke <onnamdialex@gmail.com>
2023-05-29 21:54:02 +0100

    :construction: wip ...

--- app/index.js
+++ app/index.js
@@ -1,10 +1,33 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  PopularJobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
+  //declaring the router variable to access the expo-router lib
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimensions="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.profile} dimensions="60%" />
+          ),
+        }}
+      />
+    </SafeAreaView>
   );
 };
 
