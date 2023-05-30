a6168fa882459089f76ac4af6b5a9641c46504db
Nnamdi Okeke <onnamdialex@gmail.com>
2023-05-30 16:20:02 +0100

    :zap: improve performance by fixing the custom fonts issue

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,28 @@
 import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+//to prevent the native splash screen from becoming invisible
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-  return <Stack />;
+  //to use the custom fonts, they must be required through useFonts
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  //an effect-like fnc to load the app when the fonts are loaded
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync;
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+  return <Stack onLayout={onLayoutRootView} />;
 };
 
 export default Layout;
