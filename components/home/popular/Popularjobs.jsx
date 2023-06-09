99a3f3f5e401cfba1b2ec6f6cb62cc80637e60b7
Nnamdi Okeke <onnamdialex@gmail.com>
2023-06-09 18:18:59 +0100

    :art: improve structure for Pj. wip ..

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -13,9 +13,15 @@ import { COLORS, SIZES } from "../../../constants";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
 
 const Popularjobs = () => {
+  const router = useRouter();
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text>Show all</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
