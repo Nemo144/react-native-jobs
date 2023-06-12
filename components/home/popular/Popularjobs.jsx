4f36b084f7470fb448059a54687b87b694b3bf69
Nnamdi Okeke <onnamdialex@gmail.com>
2023-06-12 23:20:25 +0100

    :construction: wip...

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -14,6 +14,9 @@ import PopularJobCard from "../../common/cards/popular/PopularJobCard";
 
 const Popularjobs = () => {
   const router = useRouter();
+
+  const isLoading = false;
+  const error = false;
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -21,6 +24,16 @@ const Popularjobs = () => {
         <TouchableOpacity>
           <Text>Show all</Text>
         </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <Flatlist />
+        )}
       </View>
     </View>
   );
