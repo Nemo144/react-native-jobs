14bd1028e0d9072b9b875a1b87d53effcb0455d0
Nnamdi Okeke <onnamdialex@gmail.com>
2023-06-30 15:30:29 +0100

    :construction: wip ..

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -20,6 +20,10 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
       <Text style={styles.companyName} numberOfLines={1}>
         {item.employer_name}
       </Text>
+
+      <View style={styles.infoContainer}>
+        <Text style={styles.jobName(selectedJob, item)}>{item.job_title}</Text>
+      </View>
     </TouchableOpacity>
   );
 };
