a957ff71504510e42e33c5ac2d62ae1f828f4800
Nnamdi Okeke <onnamdialex@gmail.com>
2023-07-05 18:26:25 +0100

    :art: Improve structure of the details page

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -20,6 +20,9 @@ import { COLORS, SIZES, icons } from "../../constants";
 import useFetch from "../../hook/useFetch";
 
 const JobDetails = () => {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {};
   const params = useSearchParams();
   const router = useRouter();
   const { data, isLoading, error, refetch } = useFetch("job-details", {
@@ -44,7 +47,35 @@ const JobDetails = () => {
           ),
           headerTitle: "",
         }}
-      ></Stack.Screen>
+      />
+
+      <>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
+          {isLoading ? (
+            <ActivityIndicator size="large" color={COLORS.primary} />
+          ) : error ? (
+            <Text>Something went wrong</Text>
+          ) : data.length === 0 ? (
+            <Text>No data</Text>
+          ) : (
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
+
+              <JobTabs />
+            </View>
+          )}
+        </ScrollView>
+      </>
     </SafeAreaView>
   );
 };
